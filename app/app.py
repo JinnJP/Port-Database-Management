01e486b8ceb02ada7e0926bf92a995721b5105d6
JinnJP <jindarat_pe@kkumail.com>
2024-02-29 20:24:10 +0700

    edited func create table

--- app/app.py
+++ app/app.py
@@ -14,6 +14,45 @@ def connector():
     }
     return config
 
+def get_column_types(src_table):
+    connection = mysql.connector.connect(**connector())
+    cursor = connection.cursor()
+    query = f"DESCRIBE src.{src_table};"
+    cursor.execute(query)
+    column_types = {column[0]: column[1] for column in cursor.fetchall()}
+    cursor.close()
+    return column_types
+
+@app.route('/create_table', methods=['GET'])
+def search():
+    src_table = request.args.get("src_table")
+    src_column = request.args.getlist("src_column")
+    dest_table = request.args.get("dest_table")
+    dest_column = request.args.getlist("dest_column")
+    connection = mysql.connector.connect(**connector())
+    cursor = connection.cursor()
+    # print("route create_table")
+    column_types = get_column_types(src_table)
+    sql_statements = manage_create_query(src_table, src_column, dest_table, dest_column, column_types)
+    # created = create_table(create_query)
+    # print("sql_statements = ",sql_statements)
+    try:
+        for sql in sql_statements:
+            cursor.execute(sql)
+            # print(f"Executed SQL: {sql};")
+        # Commit the changes (if necessary)
+        connection.commit()
+        return jsonify({'status': 'Table created!', 'query': sql_statements})
+    except mysql.connector.Error as err:
+        # Handle the error and return a JSON response
+        error_message = str(err)
+        return jsonify({'error': error_message})
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()  
+
+#Route for testing
 @app.route('/car_brand')
 def index():
     # config_db = connector_sql()
@@ -27,11 +66,11 @@ def index():
 
 @app.route('/select_dest')
 def select_dest():
-    connection_config = connector()
-    connection = mysql.connector.connect(**connection_config)
+    # connection_config = connector()
+    connection = mysql.connector.connect(**connector())
+    cursor = connection.cursor()
     try:
         # Create a cursor object to execute queries
-        cursor = connection.cursor()
         # Your SELECT query
         query = "SELECT * FROM dest.car_brand;"
         # Execute the query
@@ -45,38 +84,35 @@ def select_dest():
         cursor.close()
         connection.close()
 
-@app.route('/create_table', methods=['GET'])
-def search():
-    src_table = request.args.get("src_table")
-    src_column = request.args.getlist("src_column")
+@app.route('/desc_dest')
+def desc_dest():
     dest_table = request.args.get("dest_table")
-    dest_column = request.args.getlist("dest_column")
-    create_query = manage_query(src_table,src_column,dest_table,dest_column)
-    # created = create_table(create_query)
-    connection_config = connector()
-    connection = mysql.connector.connect(**connection_config)
-    try:
-        # Create a cursor object to execute queries
-        cursor = connection.cursor()
-        # Execute the query 
-        # cursor.execute(create_query, multi=True)
-        cursor.execute(create_query)
-        # result = cursor.fetchall()
-        return "Table created!"
-    except mysql.connector.Error as err:
-        # Handle the error and return a JSON response
-        error_message = str(err)
-        return jsonify({'error': error_message})
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
+    connection = mysql.connector.connect(**connector())
+    cursor = connection.cursor()
+    query = f"DESCRIBE dest.{dest_table};"
+    cursor.execute(query)
+    column_types = {column[0]: column[1] for column in cursor.fetchall()}
+    # result = cursor.fetchall()
+    cursor.close()
+    return column_types
+
+@app.route('/desc_src', methods=['GET'])
+def desc_src():
+    src_table = request.args.get("src_table")
+    connection = mysql.connector.connect(**connector())
+    cursor = connection.cursor()
+    query = f"DESCRIBE src.{src_table};"
+    cursor.execute(query)
+    # column_types = {column[0]: column[1] for column in cursor.fetchall()}
+    results = cursor.fetchall()
+    cursor.close()
+    # return jsonify({'result': result,'column_types': column_types})
+    return results
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4999, debug=True)
 
